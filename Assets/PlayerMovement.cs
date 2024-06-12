3a61f2c49b0bac0123d3073b7b8bcb2049801114
jaider4419 <jaiderboateng8@gmail.com>
2024-06-12 16:56:22 +0100

    Brehs

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;  
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveHorizontal, moveVertical).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        
+        rb.velocity = moveInput * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Walls")
+        {
+            Debug.Log("Wall hit");
+        }
+        
+    }
+}
